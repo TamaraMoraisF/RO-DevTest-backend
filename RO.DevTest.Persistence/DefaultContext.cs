f9d63148fafe57c892c172310cff8955734042c8
Tamara Morais Frigo <tamaramoraisfrgio@gmail.com>
2025-04-16 13:03:58 -0400

    feat: configure EF Core with PostgreSQL and apply initial migration

--- RO.DevTest.Persistence/DefaultContext.cs
+++ RO.DevTest.Persistence/DefaultContext.cs
@@ -4,16 +4,24 @@ using RO.DevTest.Domain.Entities;
 
 namespace RO.DevTest.Persistence;
 
-public class DefaultContext : IdentityDbContext<User> {
-
+public class DefaultContext : IdentityDbContext<User>
+{
     public DefaultContext() { }
 
     public DefaultContext(DbContextOptions<DefaultContext> options) : base(options) { }
 
-    protected override void OnModelCreating(ModelBuilder builder) {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5433;Database=rodevtest;User Id=postgres;Password=1234;");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
         builder.HasPostgresExtension("uuid-ossp");
         builder.ApplyConfigurationsFromAssembly(typeof(DefaultContext).Assembly);
-
         base.OnModelCreating(builder);
     }
 }
