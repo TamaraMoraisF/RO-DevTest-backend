bdbdb06ac312cbae79e075e3354a9bb40851c65a
Tamara Morais Frigo <tamaramoraisfrgio@gmail.com>
2025-04-26 00:05:51 -0400

    test: add unit tests for all pagination and analytics command validators

---
+++ RO.DevTest.Tests/Unit/Application/Features/Sale/Commands/GetSalesAnalyticsCommandValidatorTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation.TestHelper;
+using RO.DevTest.Application.Features.Sale.Commands.GetPagedSales;
+
+namespace RO.DevTest.Tests.Unit.Application.Features.Sale.Commands;
+
+public class GetSalesAnalyticsCommandValidatorTests
+{
+    private readonly GetSalesAnalyticsCommandValidator _validator = new();
+
+    [Fact]
+    public void Should_Have_Error_When_StartDate_Is_After_EndDate()
+    {
+        var command = new GetSalesAnalyticsCommand(DateTime.UtcNow.AddDays(1), DateTime.UtcNow);
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_StartDate_Is_Default()
+    {
+        var command = new GetSalesAnalyticsCommand(default, DateTime.UtcNow);
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.Start);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_EndDate_Is_Default()
+    {
+        var command = new GetSalesAnalyticsCommand(DateTime.UtcNow, default);
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.End);
+    }
+
+    [Fact]
+    public void Should_Not_Have_Error_When_Dates_Are_Valid()
+    {
+        var command = new GetSalesAnalyticsCommand(DateTime.UtcNow.AddDays(-5), DateTime.UtcNow);
+        var result = _validator.TestValidate(command);
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
